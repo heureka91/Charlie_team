237ab71517c60f8b3f4284732693c6f29fc81235
S_Tibor <68108961+heureka91@users.noreply.github.com>
2024-06-02 12:38:43 +0200

    Add files via upload

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 export const getToken = (): string | null => {
-    return localStorage.getItem('token') || sessionStorage.getItem('token');
+    return localStorage.getItem('token');
 };
 
 export const setToken = (token: string): void => {
@@ -8,5 +8,5 @@ export const setToken = (token: string): void => {
 
 export const clearToken = (): void => {
     localStorage.removeItem('token');
-    sessionStorage.removeItem('token');
+
 };
